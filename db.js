110d110873a32b7c617ae2a742695191db9cf205
Marquezzitto <marcoshmarques@gmail.com>
2025-09-17 13:01:07 -0300

    Add files via upload

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     host: '34.95.76.142', // <-- O ENDEREÇO IPV4 DIRETO DO SEU BANCO DE DADOS
-    user: 'postgres.giamvztgxpaswawapnhx',
+    user: 'postgres',
     password: 'Gilbrinks2027',
     database: 'postgres',
     port: 5432,
 });
 
-console.log('Pool de conexões [VERSÃO IP DIRETO] criado.');
+console.log('Pool de conexões [VERSÃO FINAL CORRIGIDA] criado.');
 
 module.exports = pool;
